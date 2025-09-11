6094ee530387cf3ca3bc7d4d6cb6920d993d05b3
kanishka_d <kanishkad1210@gmail.com>
2025-09-12 01:18:35 +0530

    update cloud func

--- src/processing/coarse_location.py
+++ src/processing/coarse_location.py
@@ -1,3 +1,121 @@
+'''import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+
+edge_gateways = {
+    'Zone A': [0, 0],
+    'Zone B': [100, 0],
+    'Zone C': [0, 100],
+    'Zone D': [100, 100],
+    'Zone E': [50, 50]
+} # fixed edge gateway locations
+
+# door and user position
+door_position = np.array([0, 50])
+user_pos = np.array([5, 50])  
+
+res_x = (0, 20)     # restricted near door(to place materials)
+res_y = (40, 60)
+
+material_locations = {}
+for i in range(50):
+    while True:
+        x = random.randint(0, 100) # random loaction to place materials
+        y = random.randint(0, 100)
+        if not (res_x[0] <= x <= res_x[1] and res_y[0] <= y <= res_y[1]):
+            material_locations[f"MAT{str(i+1).zfill(3)}"] = [x, y]
+            break
+
+def get_rssi(tag_position):
+    rssi_values = {}
+
+    for zone, gw_pos in edge_gateways.items():
+        distance = np.linalg.norm(tag_position - np.array(gw_pos)) # get tag's position
+        rssi = -59 - 10 * 2 * np.log10(distance + 1e-5) + np.random.normal(0, 3) # -> closer , less neg val. larger distance , positive val. and random noise
+        rssi_values[zone] = rssi
+    return rssi_values
+
+def get_coarse_location(tag_position):
+    rssi_data = get_rssi(tag_position) # max of rssi -> strogest signal
+    max_rssi = max(rssi_data.values())
+    
+    tied_zones = [zone for zone, rssi in rssi_data.items() if abs(rssi - max_rssi) <= 1.0]#to find which edge gateway has same rssi value
+    
+    if len(tied_zones) == 1:
+        coarse_location = tied_zones[0]
+
+    else: # if multiple zones there, calculate the distance from user positon and edge gateway
+        distances = [np.linalg.norm(user_pos - np.array(edge_gateways[zone])) for zone in tied_zones]
+        nearest_idx = np.argmin(distances)
+        coarse_location = tied_zones[nearest_idx]
+    
+    return coarse_location, max_rssi, rssi_data
+
+def cloud_computation(tag_position):
+    print("computing the exact location....waiting for cloud response...")
+
+def plot_all(material_positions, selected_tag_pos, coarse_zone): # plotting all values in graph
+    plt.figure(figsize=(10, 10))
+    
+    
+    for mat_id, pos in material_positions.items():
+        plt.scatter(pos[0], pos[1], c='gray', marker='o', s=50, alpha=0.6) # plot in circle shape 
+        plt.text(pos[0] + 0.5, pos[1] + 0.5, mat_id, fontsize=6)
+    
+    for zone, pos in edge_gateways.items():
+        plt.scatter(pos[0], pos[1], marker='s', s=200,
+                    color='green' if zone == coarse_zone else 'blue')
+        plt.text(pos[0] + 1, pos[1] + 1, zone, fontsize=9)
+    
+   
+    plt.scatter(door_position[0], door_position[1], c='purple', marker='D', s=150)
+    plt.text(door_position[0] + 1, door_position[1] + 1, 'Door', fontsize=10, color='purple')
+
+    plt.scatter(user_pos[0], user_pos[1], c='orange', marker='*', s=200)
+
+    plt.scatter(selected_tag_pos[0], selected_tag_pos[1], c='red', marker='x', s=150)
+    
+    plt.grid(True)
+    plt.xlim(-10, 110)
+    plt.ylim(-10, 110)
+    
+    plt.show()
+
+def simulation():
+    print("Available Material IDs:")
+    print(", ".join(list(material_locations.keys())))
+    
+    material_id = input("\nEnter Material ID (e.g., MAT001): ").strip().upper()
+    
+    if material_id in material_locations:
+        tag_pos = np.array(material_locations[material_id])
+        
+        coarse_zone, max_rssi, rssi_details = get_coarse_location(tag_pos)
+        
+        print(f"\nMaterial ID: {material_id}")
+        print(f"Material Position: {tag_pos.tolist()}")
+        print(f"RSSI values: {rssi_details}")
+        print(f"Coarse Location Zone: {coarse_zone} (Max RSSI: {max_rssi:.2f} dBm)")
+
+       # for cloud
+        c_input = input("\nDo you want to compute the exact location via Cloud? (yes/no): ").strip().lower()
+        if c_input == 'yes':
+            refined_zone = cloud_computation(tag_pos)
+            print(f"Refined location: {refined_zone}")
+        else:
+            refined_zone = None
+            print("Skipping Cloud computation.")
+
+        plot_all(material_locations, tag_pos, coarse_zone, refined_zone)
+        
+        plot_all(material_locations, tag_pos, coarse_zone)
+
+    else:
+        print(f"Material ID '{material_id}' not found!")
+
+simulation()
+'''
 import numpy as np
 import matplotlib.pyplot as plt
 import random
@@ -52,6 +170,9 @@ def get_coarse_location(tag_position):
     
     return coarse_location, max_rssi, rssi_data
 
+def cloud_computation(tag_position): # dummy cloud function
+    print("Computing the exact location....waiting for cloud response...")
+
 def plot_all(material_positions, selected_tag_pos, coarse_zone): # plotting all values in graph
     plt.figure(figsize=(10, 10))
     
@@ -94,6 +215,15 @@ def simulation():
         print(f"Material Position: {tag_pos.tolist()}")
         print(f"RSSI values: {rssi_details}")
         print(f"Coarse Location Zone: {coarse_zone} (Max RSSI: {max_rssi:.2f} dBm)")
+
+        # for cloud
+        c_input = input("\nDo you want to compute the exact location via Cloud? (yes/no): ").strip().lower()
+        if c_input == 'yes':
+            refined_zone = cloud_computation(tag_pos)
+            print(f"Refined location: {refined_zone}")
+        else:
+            refined_zone = None
+            print("Skipping Cloud computation.")
         
         plot_all(material_locations, tag_pos, coarse_zone)
     else:
